5bcea96c79592129aef031a232ca208eb218bf66
chinesesien <381640563@qq.com>
2017-09-21 06:42:52 +0800

    none

---
+++ C/zlj/sigma.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<cstring>
+#include<algorithm>
+#include<deque>
+using namespace std;
+
+int main(){
+	printf("27675\n");
+
+	return 0;
+}
